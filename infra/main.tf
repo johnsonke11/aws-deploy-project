14594a61ab396dca086f1f276b3bea9e519a76ea
Kenneth Clark Johnson <johnsonke12@outlook.com>
2026-07-05 22:10:40 -0400

    migrate terraform state to S3

--- infra/main.tf
+++ infra/main.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "s3" {
+    bucket = "flask-app-terraform-state-882282737373"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
   required_providers {
     aws = {
         source = "hashicorp/aws"
